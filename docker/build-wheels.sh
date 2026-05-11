408354a2d16995346225abe347adabad38b529d6
David Hernandez <david.hernandez@tobii.com>
2026-05-11 09:09:03 +0200

    Bump aimrocks

--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -11,12 +11,12 @@ python -m pip install setuptools
 python -m build
 pip install dist/*${PYTHON_VERSION}*.whl
 pip install git+https://github.com/aimhubio/auditwheel.git@include-exclude-new
-export LIB_DIR=`python -c "from aimrocks import lib_utils; print(lib_utils.get_lib_dir())"`
-export LIBS_BUNDLED=`ls ${LIB_DIR}/ \
+export LIB_DIR=$(python -c "from aimrocks import lib_utils; print(lib_utils.get_lib_dir())")
+export LIBS_BUNDLED=$(ls ${LIB_DIR}/ \
   | grep .so \
   | sed -r 's/^(lib.*?\.so)\.*?$/\1/g' \
   | uniq \
-  | paste -s -d','`
+  | paste -s -d',')
 export LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
 auditwheel repair \
   --exclude $LIBS_BUNDLED --wheel-dir multilinux_dist \
